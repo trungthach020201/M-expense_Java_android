719a882e83678238b33882706374282b74eaca25
phuochao091101 <ht0346nt@gmail.com>
2022-10-15 13:31:56 +0700

    fix delete Trip in android

--- app/src/main/java/com/comp1786/m_expense/Trip/DetailTripFragment.java
+++ app/src/main/java/com/comp1786/m_expense/Trip/DetailTripFragment.java
@@ -76,7 +76,7 @@ public class DetailTripFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }s
+        }
 
     }
 
